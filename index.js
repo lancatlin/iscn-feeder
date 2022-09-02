529dfdbb1e3122a156d0abd32ea93472c0db1693
Justin Lin <wancat@wancat.cc>
2022-09-02 12:32:39 +0800

    :bug: Change port by ENV

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import iscnFeed from './feed.js';
 import api from './api.js';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/feed', async (req, res) => {
   try {
